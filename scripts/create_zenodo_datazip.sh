4269260ae2ebde34476b381ed619da3930ff9391
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2021-11-22 19:23:51 +0100

    \ Update zenodo dataset creation script

--- scripts/create_zenodo_datazip.sh
+++ scripts/create_zenodo_datazip.sh
@@ -4,9 +4,7 @@
 python anonymize_data.py
 
 ## Zip data
-zip -r -D ../processed_data/EUREC4A_ManualClassifications_MergedClassifications.zarr.zip ../processed_data/EUREC4A_ManualClassifications_MergedClassifications.zarr
-zip -r -D ../processed_data/EUREC4A_ManualClassifications_l3_IR.zarr.zip ../processed_data/EUREC4A_ManualClassifications_l3_IR.zarr
-zip -r -D ../processed_data/EUREC4A_ManualClassifications_l3_VIS.zarr.zip ../processed_data/EUREC4A_ManualClassifications_l3_VIS.zarr
-zip -r -D ../processed_data/EUREC4A_ManualClassifications_l3_albedo.zarr.zip ../processed_data/EUREC4A_ManualClassifications_l3_albedo.zarr
+cd ../processed_data/
 
-zip -D ../zenodo/EUREC4A_ManualCloudClassificationData.zip ../processed_data/EUREC4A_ManualClassifications_MergedClassifications.zarr.zip ../processed_data/EUREC4A_ManualClassifications_l3_VIS.nc ../processed_data/EUREC4A_ManualClassifications_l3_IR.nc ../processed_data/EUREC4A_ManualClassifications_l3_albedo.nc ../processed_data/EUREC4A_ManualClassifications_l1_anonymized.nc ../zooniverse_raw/sugar-flower-fish-or-gravel-subjects.csv ../zooniverse_raw/sugar-flower-fish-or-gravel-classifications_anonymized.csv
+zip -r -D -j ../zenodo/EUREC4A_ManualCloudClassificationData.zip EUREC4A_ManualClassifications_l2.zarr EUREC4A_ManualClassifications_l3_VIS.zarr EUREC4A_ManualClassifications_l3_IR.zarr EUREC4A_ManualClassifications_l3_ICON.zarr EUREC4A_ManualClassifications_l1_anonymized.nc ../zooniverse_raw/sugar-flower-fish-or-gravel-subjects.csv ../zooniverse_raw/sugar-flower-fish-or-gravel-classifications_anonymized.csv
+
