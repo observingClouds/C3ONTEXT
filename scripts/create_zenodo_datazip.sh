54f6b41f5ee1f1555f0a957fc9065c7f831b1d74
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2021-11-24 15:25:16 +0100

    - relative paths in zenodo zip

--- scripts/create_zenodo_datazip.sh
+++ scripts/create_zenodo_datazip.sh
@@ -4,7 +4,9 @@
 python anonymize_data.py
 
 ## Zip data
-cd ../processed_data/
+cd ../
 
-zip -r -D -j ../zenodo/EUREC4A_ManualCloudClassificationData.zip EUREC4A_ManualClassifications_l2.zarr EUREC4A_ManualClassifications_l3_VIS.zarr EUREC4A_ManualClassifications_l3_IR.zarr EUREC4A_ManualClassifications_l3_ICON.zarr EUREC4A_ManualClassifications_l1_anonymized.nc ../zooniverse_raw/sugar-flower-fish-or-gravel-subjects.csv ../zooniverse_raw/sugar-flower-fish-or-gravel-classifications_anonymized.csv
+mkdir zenodo
+
+zip -r -D zenodo/EUREC4A_ManualCloudClassificationData.zip processed_data/EUREC4A_ManualClassifications_l2.zarr processed_data/EUREC4A_ManualClassifications_l3_VIS_instant.zarr processed_data/EUREC4A_ManualClassifications_l3_VIS_daily.zarr processed_data/EUREC4A_ManualClassifications_l3_IR_instant.zarr processed_data/EUREC4A_ManualClassifications_l3_IR_daily.zarr processed_data/EUREC4A_ManualClassifications_l3_albedo_instant.zarr processed_data/EUREC4A_ManualClassifications_l3_albedo_daily.zarr processed_data/EUREC4A_ManualClassifications_l1_anonymized.nc zooniverse_raw/sugar-flower-fish-or-gravel-subjects.csv zooniverse_raw/sugar-flower-fish-or-gravel-classifications_anonymized.csv
 
