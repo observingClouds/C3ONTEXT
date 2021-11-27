0cfe1240c37fd2b0ec502789430d2d6729bbe622
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2021-11-27 14:46:54 +0100

    \ WIP: zenodo archive with parser for different classifications

--- scripts/anonymize_data.py
+++ scripts/anonymize_data.py
@@ -4,20 +4,45 @@ Script to anonymize the classifications
 The user names can be deleted and only user ids
 are kept.
 """
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-c', '--configfile', help='Config file containing settings and paths for conversion', required=False, default='config.yaml')
+    parser.add_argument('-e', '--classification', help="Choose which classification of those described in the configfile should be used",
+                        required=True, default=None)
+    parser.add_argument('-v', '--verbose', metavar="DEBUG",
+                        help='Set the level of verbosity [DEBUG, INFO, WARNING, ERROR]',
+                        required=False, default="INFO")
+
+    args = vars(parser.parse_args())
+
+    return args
+
+
+args = get_args()
+
+# Load config
+from omegaconf import OmegaConf as oc
+conf = oc.load(args['configfile'])
+
 
 import pandas as pd
 import xarray as xr
 
-raw_data_filename_input = '../zooniverse_raw/sugar-flower-fish-or-gravel-classifications.csv'
-raw_data_filename_output = '../zooniverse_raw/sugar-flower-fish-or-gravel-classifications_anonymized.csv'
+classification = args['classification']
 
-level1_data_filename_input = '../processed_data/EUREC4A_ManualClassifications_l1.nc'
-level1_data_filename_output = '../processed_data/EUREC4A_ManualClassifications_l1_anonymized.nc'
+raw_data_filename_input = conf[classification].input.classifications_file
+raw_data_filename_output = conf[classification].input.classifications_file_anonymous
+
+level1_data_filename_input = conf[classification].level1.fn_netcdf
+level1_data_filename_output = conf[classification].level1.fn_netcdf_anonymous
 
 # Anonymize raw data
 
 df = pd.read_csv(raw_data_filename_input)
-df = df.iloc[38003:]  # remove data from other classification days
+if classification == "EUREC4A":
+    df = df.iloc[38003:]  # remove data from other classification days
 del df['user_name']
 df.to_csv(raw_data_filename_output)
 
